f9105f5d74a0b59c99ff83b5ce54b83a28c6a532
Bethany Chance <bethanywchance@gmail.com>
2020-05-17 18:44:55 -0400

    Initial Commit

---
+++ Official App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Official App
+//
+//  Created by CCP019 on 5/17/20.
+//  Copyright © 2020 BethanyC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
